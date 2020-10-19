bf696bd3a935c02c09862dcb12c499c15a524374
Moojjcorn <linus.holmgren@elev.ga.ntig.se>
2020-10-19 12:54:14 +0200

    Create Övning 3.1.py

---
+++ Övning 3.1.py
@@ -0,0 +1,10 @@
+abonnemang = int(input("Hur många samtals minuter per månad? ")) #Ber om en input i data typen int
+
+if abonnemang <= 33: #Om inputen är 33 eller mindre så kommer den att skriva ut att man bör använda kontant abonnemanget
+    print("Välj kontant abonnemanget")
+elif abonnemang > 33 and abonnemang < 66: #Om inputen var mellan 33 och 66 så skriver den att man ska använda normala abonnemanget
+    print("Välj normal abonnemanget")
+elif abonnemang <= 66: #Om man skriver något högre än 66 så kommer den skriva ut att man ska använda sig av plus abonnemanget
+    print("Välj plus abonnemanget")
+else: #Elsen kommer att skrivas ut om man inte skriver ett giltigt tal. Elsen behövs inte ens användas för att det kommer crasha om du inte skriver ett heltal. 
+    print('Ange ett giltigt tal!')
