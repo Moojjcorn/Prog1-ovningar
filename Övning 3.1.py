64ced63627ff787d21676784482bb1d38664d2ad
Moojjcorn <45688077+Moojjcorn@users.noreply.github.com>
2020-11-30 16:40:59 +0100

    Update Övning 3.1.py

--- Övning 3.1.py
+++ Övning 3.1.py
@@ -4,5 +4,5 @@ if abonnemang <= 33: #Om inputen är 33 eller mindre så kommer den att skriva u
     print("Välj kontant abonnemanget")
 elif abonnemang > 33 and abonnemang < 66: #Om inputen var mellan 33 och 66 så skriver den att man ska använda normala abonnemanget
     print("Välj normal abonnemanget")
-elif abonnemang <= 66: #Om man skriver något högre än 66 så kommer den skriva ut att man ska använda sig av plus abonnemanget
+elif abonnemang >= 66: #Om man skriver något högre än 66 så kommer den skriva ut att man ska använda sig av plus abonnemanget
     print("Välj plus abonnemanget")
